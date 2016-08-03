b5998d8e7eb49853ca565c00e7ec211df65ba2ad
ajaxboy <cjxxi21@gmail.com>
2016-08-03 09:38:50 -0700

    i dont think anybody uses the older versions these days don't need this check, if they do they need to upgrade

--- cjax/plugins/autocomplete/autocomplete.js
+++ cjax/plugins/autocomplete/autocomplete.js
@@ -18,15 +18,6 @@ CJAX.importFile({
 
 function autocomplete(url , fulll_load, limit)
 {
-	version = CJAX.version.replace(/[^0-9\.].*/,'');
-
-	if(parseFloat(version) < 5.3) {
-		alert('Sorry, Autocomplete Plugin requires Cjax 5.3 or greater.');
-
-		//return this.clear();//remove keyup event only in > 5.3
-		return CJAX._EventCache.flushElement(this.element); ////remove keyup event
-	}
-
 	CJAX.ajaxSettings.cache = true;
 
 	var element = CJAX.$(CJAX.xml('element_id',CJAX._plugins['autocomplete']));
