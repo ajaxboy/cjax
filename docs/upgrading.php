fb020dc3e94973db9c0edb5632696e8647d1b90c
ajaxboy <cjxxi21@gmail.com>
2016-08-14 19:35:49 -0700

    docs were moved

--- docs/upgrading.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'ajax.php';
-?>
-<h4>Upgrading</h4>
-Using an older version?. To upgrade simply download the newest version and overwrite all Cjax files,  in most cases that should be enough to upgrade to a newer version.<br />
-To be sure, see release notes or <a target='_blank' href='http://cjax.svn.sourceforge.net/viewvc/cjax/CHANGELOG.txt?revision=1&view=markup'>changeLog</a> to see if there are any changes that would affect a previous release.
