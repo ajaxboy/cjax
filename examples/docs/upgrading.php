9790e7a570ca71ec9da31f40965627d3717f9e33
ajaxboy <cjxxi21@gmail.com>
2016-08-16 12:46:24 -0700

    upgrading docs

--- examples/docs/upgrading.php
+++ examples/docs/upgrading.php
@@ -1,7 +1,80 @@
 <?php
+//core file, reference..
+require_once "../../ajax.php";
 
-require 'ajax.php';
+$ajax = ajax();
+
+
 ?>
-<h4>Upgrading</h4>
-Using an older version?. To upgrade simply download the newest version and overwrite all Cjax files,  in most cases that should be enough to upgrade to a newer version.<br />
-To be sure, see release notes or <a target='_blank' href='http://cjax.svn.sourceforge.net/viewvc/cjax/CHANGELOG.txt?revision=1&view=markup'>changeLog</a> to see if there are any changes that would affect a previous release.
+<!doctype html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../resources/css/user_guide.css" media="all">
+    <title>Upgrading</title>
+    <?php echo $ajax->init();?>
+</head>
+<body>
+<header>
+    <div style='padding: 15px;'>
+        <a href='http://cjax.sourceforge.net'><img src='http://cjax.sourceforge.net/media/logo.png' border=0/></a>
+    </div>
+</header>
+<!-- START NAVIGATION -->
+<div id="nav"><div id="nav_inner"></div></div>
+<div id="nav2"><a name="top">&nbsp;</a></div>
+<div id="masthead">
+    <table cellpadding="0" cellspacing="0" border="0" style="width:100%">
+        <tr>
+            <td><h1>Cjax Framework</h1></td>
+            <td id="breadcrumb_right"><a href="#">Demos</a></td>
+        </tr>
+    </table>
+</div>
+<!-- END NAVIGATION -->
+
+
+
+<!-- START BREADCRUMB -->
+<table cellpadding="0" cellspacing="0" border="0" style="width:100%">
+    <tr>
+        <td id="breadcrumb">
+            <a href="http://cjax.sourceforge.net/">Project Home</a> &nbsp;&#8250;&nbsp;
+            <a href="../">Demos</a> &nbsp;&#8250;&nbsp;
+            Upgrading
+        </td>
+        <td id="searchbox"><form method="get" action="http://www.google.com/search"><input type="hidden" name="as_sitesearch" id="as_sitesearch" value="cjax.sourceforge.net/" />Search Project User Guide&nbsp; <input type="text" class="input" style="width:200px;" name="q" id="q" size="31" maxlength="255" value="" />&nbsp;<input type="submit" class="submit" name="sa" value="Go" /></form></td>
+    </tr>
+</table>
+<!-- END BREADCRUMB -->
+
+<br clear="all" />
+
+<div id="content">
+
+
+    <!-- Text -->
+    <h4>Upgrading</h4>
+    Using an older version?. To upgrade simply download the newest version and overwrite all Cjax files,  in most cases that should be enough to upgrade to a newer version.<br />
+    To be sure, see release notes or <a target='_blank' href='http://cjax.svn.sourceforge.net/viewvc/cjax/CHANGELOG.txt?revision=1&view=markup'>changeLog</a> to see if there are any changes that would affect a previous release.
+
+
+
+    <br />
+</div>
+<!-- END CONTENT -->
+
+<div id="myfooter">
+    <p>
+        Previous Topic:&nbsp;&nbsp;<a href="#">Previous Class</a>
+        &nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+        <a href="#top">Top of Page</a>&nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+        <a href="http://cjax.sourceforge.net/examples">Demos Home</a>&nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+        <!-- Next Topic:&nbsp;&nbsp;<a href="#">Next Class</a> -->
+    </p>
+    <p>
+        <a href="http://codeigniter.com">CodeIgniter</a> &nbsp;&middot;&nbsp; Copyright &#169; 2006 - 2012 &nbsp;&middot;&nbsp;
+        <a href="http://cjax.sourceforge.net/">Cjax</a>
+    </p>
+</div>
+
+</body>
+</html>
