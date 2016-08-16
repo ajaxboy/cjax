208798fb244d390d11c35a7a532febfac7779fc4
ajaxboy <cjxxi21@gmail.com>
2016-08-16 03:04:29 -0700

    testing ci integration

--- cjax/integration.php
+++ cjax/integration.php
@@ -44,7 +44,7 @@ function moveSelf()
 {
     $cwd = dirname(__FILE__);
     $dir = dirname($cwd);
-    rcopy(sprintf('%s/cjax/integration/default/ajax.php', $dir), sprintf('%s/ajax.php', $dir));
+    copy(sprintf('%s/cjax/integration/codeigniter/ajax.php', $dir), sprintf('%s/ajax.php', $dir));
     rename(sprintf('%s/cjax/',$dir));
 }
 if(is_file('composer.json')) {
