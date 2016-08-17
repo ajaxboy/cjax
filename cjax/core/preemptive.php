b26016b64a385fc76793a20463642056f239a902
ajaxboy <cjxxi21@gmail.com>
2016-08-16 21:36:05 -0700

    restore old

--- cjax/core/preemptive.php
+++ cjax/core/preemptive.php
@@ -13,14 +13,12 @@
  *   Written by: CJ Galindo
  *   Website: http://cjax.sourceforge.net                     $
  *   Email: cjxxi@msn.com
- *   Date: 2/12/2007                           $
- *   File Last Changed:  10/05/2013            $
  **####################################################################################################    */
+
 
 
 include_once 'cjax_config.php';
 $ajax = ajax();
-
 
 /**
  * Handle mod_rewrite redirects
@@ -31,12 +29,18 @@ if(isset($_SERVER['REDIRECT_QUERY_STRING']) && $_SERVER['REDIRECT_QUERY_STRING']
 	$_SERVER['QUERY_STRING'] = ltrim($_SERVER['PATH_INFO'],'/');
 }
 
+$file = 'ajax.php';
+if(isset($_SERVER['SCRIPT_NAME'])) {
+	$file = preg_replace("/.+\//",'', ltrim($_SERVER['SCRIPT_NAME'],'/'));
+}
+if(defined('AJAX_FILE') && AJAX_FILE !=$file) {
+	return true;
+}
 
 /**
  * Handle friendly URLS
  */
-if((isset($_SERVER['QUERY_STRING']) && $query = $_SERVER['QUERY_STRING'])) {
-
+if(isset($_SERVER['QUERY_STRING']) && $query = $_SERVER['QUERY_STRING']) {
 	$packet = explode('/' ,rtrim($query,'/'));
 	if(count($packet) == 1) {
 		$is_plugin = $packet[0];
@@ -46,49 +50,38 @@ if((isset($_SERVER['QUERY_STRING']) && $query = $_SERVER['QUERY_STRING'])) {
 			}
 		}
 	}
-	$allowed = array ('test');
-	$controller = $function = null;
-
-	if($packet && count(array_keys($packet)) >= 2 && $packet[0] && $packet[1]) {
-		$controller  = $packet[0];
-		$function 	 = $packet[1];
-		if(count(array_keys($packet)) > 2) {
-			unset($packet[0]);
-			unset($packet[1]);
-			if($packet){
-				$params = range('a','z');
-				foreach($packet as $k  => $v) {
-					$_REQUEST[current($params)] = $v;
-					next($params);
-				}
-			}
-		}
-	} else  {
-		if(!$ajax->input('controller')) {
-			if(count($packet)==1) {
-				$url = explode('&',$_SERVER['QUERY_STRING']);
-				if(count($url) ==1){
-					$controller = $packet[0];
-				}
-			}
-		}
-	}
-
 	if($ajax->isAjaxRequest() || defined('AJAX_VIEW') ) {
-		$_REQUEST['controller'] = $controller;
-		$_REQUEST['function'] = $function;
+		if($packet && count(array_keys($packet)) >= 2 && $packet[0] && $packet[1]) {
+			$_REQUEST['controller'] = $packet[0];
+			$_REQUEST['function'] = $packet[1];
+			$_REQUEST['cjax'] = time();
+			if(count(array_keys($packet)) > 2) {
+				unset($packet[0]);
+				unset($packet[1]);
+				if($packet){
+					$params = range('a','z');
+					foreach($packet as $k  => $v) {
+						$_REQUEST[current($params)] = $v;
+						next($params);
+					}
+				}
+			}
+		} else  {
+			if(!$ajax->input('controller')) {
+				if(count($packet)==1) {
+					$url = explode('&',$_SERVER['QUERY_STRING']);
+					if(count($url) ==1){
+						$_REQUEST['controller'] = $packet[0];
+					}
+				}
+			}
+		}
 	}
-	$ajax->controller 	= 	$controller;
-	$ajax->function 	= 	$function;
 }
 
 if(!$ajax->isAjaxRequest()) {
-
-	if(count(array_keys(debug_backtrace(false))) == 1
-		|| (defined('AJAX_FILE') && preg_replace('#.*\/#','', $_SERVER['SCRIPT_NAME']) == AJAX_FILE)) {
-
-		if(!defined('AJAX_VIEW') || $ajax->controller !='test') {
-			exit("Security Error. You cannot access this file directly.");
-		}
+	if(count(array_keys(debug_backtrace(false))) == 1 && !defined('AJAX_VIEW')) {
+		exit("Security Error. You cannot access this file directly.");
 	}
 }
+
