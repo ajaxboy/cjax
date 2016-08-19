5e1050a5624fb2bc5561b7bbc893933b02a83c56
ajaxboy <cjxxi21@gmail.com>
2016-08-19 04:46:57 -0700

    update

--- examples/ajax.php
+++ examples/ajax.php
@@ -1,12 +1,10 @@
-<?php if(!file_exists($f = '../ajax.php')):?>
+<?php
 
-	"Cjax Framework was not detected in the parent directory. Make sure the framework is placed in the upper directory.
-	<br />
-	You may download the framework in the official  repository:<b>Download</b>
-	<a href='http://sourceforge.net/projects/cjax/'>http://sourceforge.net/projects/cjax/</a>
+if(!file_exists($f = '../ajax.php')):
+	include 'resources/html/not_found.html';
+	exit(0);
+endif;
 
-<?php exit(0);?>
-<? endif; ?>
 define('AJAX_CD','examples/controllers');
 
 chdir(dirname(__FILE__));
