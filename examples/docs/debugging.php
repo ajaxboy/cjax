3fba19368a3dbd1e4f03dd99555e59f69bcdc839
ajaxboy <cjxxi21@gmail.com>
2016-08-21 08:13:58 -0700

    debuggin info

--- examples/docs/debugging.php
+++ examples/docs/debugging.php
@@ -38,7 +38,7 @@ $ajax = ajax();
     <tr>
         <td id="breadcrumb">
             <a href="http://cjax.sourceforge.net/">Project Home</a> &nbsp;&#8250;&nbsp;
-            <a href="./">Demos</a> &nbsp;&#8250;&nbsp;
+            <a href="../">Demos</a> &nbsp;&#8250;&nbsp;
             Debugging
         </td>
         <td id="searchbox"><form method="get" action="http://www.google.com/search"><input type="hidden" name="as_sitesearch" id="as_sitesearch" value="cjax.sourceforge.net/" />Search Project User Guide&nbsp; <input type="text" class="input" style="width:200px;" name="q" id="q" size="31" maxlength="255" value="" />&nbsp;<input type="submit" class="submit" name="sa" value="Go" /></form></td>
@@ -77,56 +77,80 @@ $ajax = ajax();
 
     <p>
         Particularly, there are certain things to watch out for, coming out of the console if you are testing any type of ajax call.
-        <p class="note">
-            Keep in mind that web-inspector include many tabs. Cjax information can be found in the response tab, 95% of the time.
-            The network tab, some times is useful as well to get information about whether a file has been loaded, or if a file is has been hard-cached.
-        </p>
-        <br />
-        <ul>
-        <li>
-            Request status response
-            <ol>
-               <li>
-                   This is a number status the server responds with depending on how the request went. 404, indicating that the controller file was not found.
-                    500 indicating a server error, and there are other types of responses you may see.
-                    If there is a php error, the console always will give you the error message, unless you have error messaging completly turned off in your server settings.
-                   If you can't find the error on the console, the next place to look is your apache log files.
-               </li>
-            </ol>
-        </li>
-        <li>
-            Console log
-            <ol>
-                <li>
-                    If there is an error, for instance produced some where in the Cjax JS library, or in a plugin, the error will show up in the
-                    console. This error can give you explicit information of why something didn't work.
-                </li>
-                <li>
-                    A plugin's dependencies could trigger one of the console errors. Dependencies are encapsulated within a plugin, they are loaded
-                    dynamically from the plugin directory by the plugin js file in cjax/plugins/[plugin-name], dependecies be found within the same directory.
-                </li>
-            </ol>
-        </li>
-        <li>
-            No Error
-            <ol>
-                <li>
-                    If you are looking at the console, you click on the URL, and view the response, and you do not see an error
-                    (and the response status is 200)  - this could be something
-                    within the scope of your ajax controller.  Try an exit test  -  eg:  exit("It's getting here"); , then go to back to the console and
-                    if you see this message it's telling you that there is no problem, and the response you expect, is not a Cjax error, rather
-                    a product of your code or lack of.
-                </li>
-            </ol>
-        </li>
-           <li>
-               Plugin Errors
-               <ol>
-                   If you are within a plugin, you can use CJAX.debug = true;,  to enable further logs to show up in the console.
-               </ol>
-           </li>
+    </p>
+
+    <p class="note">
+        Keep in mind that web-inspector include many tabs. Cjax information can be found in the response tab, 95% of the time.
+        The network tab, some times is useful as well to get information about whether a file has been loaded, or if a file is has been hard-cached.
+    </p>
+    <br />
+    <P>
+        A simple look at this can give you so much information, and can help you be on your way to focus on building more things and spend
+        less time wondering what went wrong.
+    </P>
+    <p>
+        <img src="../resources/images/console_error.png" />
+        <ul style=" list-style-type: decimal;">
+            <li>
+                1 Information about the URL, including any parameters passed
+            </li>
+            <li>
+                2 Response Status, it could be a forbidden status (403) or a server error (500),  this information is useful for you to know where to look next.
+            </li>
+            <li>
+                3  Response Tab
+            </li>
+            <li>
+                4 Response,  to get this information click the URL being displayed. If an error occured, most of the time you will be able to see that error here. That error
+                is the key to keep you up and running as it includes what went wrong, and where you need to look to fix it.
+            </li>
         </ul>
     </p>
+    <br />
+    <ul>
+    <li>
+        Request status response
+        <ol>
+           <li>
+               This is a number status the server responds with depending on how the request went. 404, indicating that the controller file was not found.
+                500 indicating a server error, and there are other types of responses you may see.
+                If there is a php error, the console always will give you the error message, unless you have error messaging completly turned off in your server settings.
+               If you can't find the error on the console, the next place to look is your apache log files.
+           </li>
+        </ol>
+    </li>
+    <li>
+        Console log
+        <ol>
+            <li>
+                If there is an error, for instance produced some where in the Cjax JS library, or in a plugin, the error will show up in the
+                console. This error can give you explicit information of why something didn't work.
+            </li>
+            <li>
+                A plugin's dependencies could trigger one of the console errors. Dependencies are encapsulated within a plugin, they are loaded
+                dynamically from the plugin directory by the plugin js file in cjax/plugins/[plugin-name], dependecies be found within the same directory.
+            </li>
+        </ol>
+    </li>
+    <li>
+        No Error
+        <ol>
+            <li>
+                If you are looking at the console, you click on the URL, and view the response, and you do not see an error
+                (and the response status is 200)  - this could be something
+                within the scope of your ajax controller.  Try an exit test  -  eg:  exit("It's getting here"); , then go to back to the console and
+                if you see this message it's telling you that there is no problem, and the response you expect, is not a Cjax error, rather
+                a product of your code or lack of.
+            </li>
+        </ol>
+    </li>
+       <li>
+           Plugin Errors
+           <ol>
+               If you are within a plugin, you can use CJAX.debug = true;,  to enable further logs to show up in the console.
+           </ol>
+       </li>
+    </ul>
 
 
    <p>
