03690eb0dc6f9b29b2791558912a3e3e940d289a
ajaxboy <cjxxi21@gmail.com>
2016-08-22 17:21:26 -0700

    changelog link

--- examples/docs/upgrading.php
+++ examples/docs/upgrading.php
@@ -54,7 +54,7 @@ $ajax = ajax();
     <!-- Text -->
     <h4>Upgrading</h4>
     Using an older version?. To upgrade simply download the newest version and overwrite all Cjax files,  in most cases that should be enough to upgrade to a newer version.<br />
-    To be sure, see release notes or <a target='_blank' href='http://cjax.svn.sourceforge.net/viewvc/cjax/CHANGELOG.txt?revision=1&view=markup'>changeLog</a> to see if there are any changes that would affect a previous release.
+    To be sure, see release notes or <a target='_blank' href='https://github.com/ajaxboy/cjax/blob/master/cjax/CHANGELOG.txt'>changeLog</a> to see if there are any changes that would affect a previous release.
 
 
 
